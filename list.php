72d32f7b83c47c4e3b32fc9fb56676fbe7dae9ef
hiphop200199 <hiphop200199@gmail.com>
2024-10-07 16:48:12 +0800

    first commit

---
+++ list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product Management System</title>
+</head>
+<body>
+    <h1>Management.</h1>
+    <form action="" method="post">
+        <input type="text" name="" id="" placeholder="account...">
+        <input type="password" name="" id="" placeholder="password...">
+        <button type="submit">login</button>
+        <a href="">register</a>
+    </form>
+    <footer></footer>
+</body>
+</html>
