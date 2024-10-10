6649909d21140989d1e71fa8df515a91b2e25e01
hiphop200199 <hiphop200199@gmail.com>
2024-10-10 18:01:20 +0800

    create index layout

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <?php include "./parts/head.php"; ?>
+
 <body>
-<h1>Management.</h1>
-    <form action="/auth/login.php" method="post">
-        <input type="text" name="account" id="" placeholder="account...">
-        <input type="password" name="password" id="" placeholder="password...">
-        <button type="submit">login</button>
-        <a href="/register.php">register</a>
-    </form>
+    <div id="box">
+        <h1 id="index-title">Management.</h1>
+        <form id="login-form" action="/auth/login.php" method="post">
+            <input type="text" name="account" id="" placeholder="account...">
+            <input type="password" name="password" id="" placeholder="password...">
+            <section>
+                <button type="submit">login</button>
+                <a href="/register.php">register</a>
+            </section>
+        </form>
+        <div id="circle"></div>
+    </div>
 </body>
+
 </html>
